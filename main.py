8ed3d545870cd3a161b4d0ee501958dbfa38cabf
Dilip0552 <suthardilip.eis@gmail.com>
2025-02-12 14:09:27 +0530

    Backend Updated

---
+++ main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import pymongo
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class UserData(BaseModel):
+    title:str
+    by:str
+    more_details:str
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+@app.post("/add_task")
+def add_task(data:UserData):
+    try:
+        client=pymongo.MongoClient(MONGO_URI)
+        db=client["To-Do"]
+        collection=db["details"]
+        collection.insert_one(data.dict())
+        return {"message":"Data Saved Successfully!","data":data.dict()}
+    except Exception as e:
+        return {"message":"An error occurred"}
+
+@app.get("/my_tasks")
+def refresh_tasks():
+    try:
+        client=pymongo.MongoClient(MONGO_URI)
+        db=client["To-Do"]
+        collection=db["details"]
+        data = list(collection.find({}, {"_id": 0}))
+        if not data: 
+            return JSONResponse(content={"message": "No tasks found"}, status_code=404)
+
+        return JSONResponse(content={"tasks": data})  
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+@app.get("/my_tasks/{title}")
+def getItem(title):
+    try:
+        client=pymongo.MongoClient(MONGO_URI)
+        db=client["To-Do"]
+        collection=db["details"]
+        one=collection.find_one({"title":title})
+        one["_id"] = str(one["_id"])
+        if not one: 
+            return JSONResponse(content={"message": "Task not found"}, status_code=404)
+
+        return one
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    
+@app.put("/update")
+def update_it(data: UserData):
+    try:
+        client = pymongo.MongoClient(MONGO_URI)
+        db = client["To-Do"]
+        collection = db["details"]
+        result = collection.update_one({"title": data.title}, {"$set": data.dict()})
+        if result.matched_count == 0:
+            return JSONResponse(content={"message": "Task not found"}, status_code=404)
+        return {"message": "Task updated successfully"}
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+@app.delete("/delete/{title}")
+def delete_it(title):
+    try:
+        client = pymongo.MongoClient(MONGO_URI)
+        db = client["To-Do"]
+        collection = db["details"]
+        collection.delete_one({"title":title})
+        return {"message": "Task deleted successfully"}
+        
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
--- script.js
+++ script.js
@@ -4,12 +4,64 @@ function new_task(){
     dialog.style.visibility="visible"
     
 }
-const dialog=document.getElementById("app")
+// const dialog=document.getElementById("app")
 
-dialog.addEventListener("volumechange",function myFucn(){
-    alert("File Dropped");
-    console.log("Hello")
-})
+// dialog.addEventListener("volumechange",function myFucn(){
+//     alert("File Dropped");
+//     console.log("Hello")
+// })
+
+
+async function send_data(title,by,more_details){
+    const response = await fetch("http://127.0.0.1:8000/add_task",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({"title":title,"by":by,"more_details":more_details})
+    });
+
+    const result = await response.json();
+    alert(result.message);
+    console.log(result.data)
+
+}
+async function edit_task(title){
+    const e_title=document.getElementById("e_title")
+    const e_by=document.getElementById("e_by-name")
+    const e_more_details=document.getElementById("e_more-details")
+    const response = await fetch(`http://127.0.0.1:8000/my_tasks/${title}`);
+    const data = await response.json();
+    // console.log(data)
+    e_title.value=data.title;
+    e_by.value=data.by;
+    e_more_details.value=data.more_details;
+    
+    
+    el=document.getElementById("edit-task")
+    el.style.visibility="visible"
+
+
+
+}
+async function save(){
+    const e_title=document.getElementById("e_title").value;
+    const e_by=document.getElementById("e_by-name").value;
+    const e_more_details=document.getElementById("e_more-details").value;
+    dict={"title":e_title, "by":e_by,"more_details":e_more_details}
+    const response = await fetch(`http://127.0.0.1:8000/update`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dict)
+    });
+    const data = await response.json(); 
+    console.log(data)
+    // alert("Saved")
+    el=document.getElementById("edit-task")
+    el.style.visibility="hidden"
+}
 
 var no_of_tasks=1
 function add_task(){
@@ -32,10 +84,11 @@ function add_task(){
     <p>${more_details}</p>
     </div>
     </div>
-    <img src="editing.png" alt="edit" id="edit">
+    <img src="editing.png" alt="edit" id="edit" onclick="edit_task(${title})>
     <img src="WhatsApp_Image_2025-01-02_at_11.41.14_6b45f130-removebg-preview.png" alt="tick" id="tick">
-    <img src="delete.png" id ="delete" alt="delete" onclick="delete_it('task${no_of_tasks}')">
+    <img src="delete.png" id ="delete" alt="delete" onclick="delete_it('task${no_of_tasks}','${title}')">
     </div>`
+    send_data(title,by,more_details)
     title=""
     by=""
     tag=""
@@ -52,14 +105,64 @@ function add_task(){
 
 }
 
-function delete_it(id){
+async function delete_it(id,title){
     element1=document.getElementById(id);
     element1.remove();
     element2=document.getElementById(`hr-${id}`);
     element2.remove();
-    // tasks=document.getElementById("tasks")
-    // console.log(tasks.innerHTML)
-    // console.log(tasks.innerHTML.length)
-    // tasks.innerHTML=tasks.innerHTML.slice(0,(tasks.innerHTML.length-4))
-    // console.log(element)
+    const response = await fetch(`http://127.0.0.1:8000/delete/${title}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+        // body: JSON.stringify(dict)
+    });
+    const data = await response.json(); 
+    console.log(data)
 }
+function add_task_bh(dict){
+    tasks=document.getElementById("tasks")
+    tasks.innerHTML+=`<hr id="hr-task${no_of_tasks}">
+    <div id="task${no_of_tasks}" class="task">
+    <input type="checkbox" name="task${no_of_tasks}" class="checkbox">
+    <div class="content">
+    <p>${dict.title}</p>
+    <p id="by">By ${dict.by}</p>
+    <div id="details">
+    <h2>${dict.title}</h2>
+    <span>By ${dict.by}</span>
+    <hr>
+    <br>
+    <p>${dict.more_details}</p>
+    </div>
+    </div>
+    <img src="editing.png" alt="edit" id="edit" onclick="edit_task('${dict.title}')">
+    <img src="WhatsApp_Image_2025-01-02_at_11.41.14_6b45f130-removebg-preview.png" alt="tick" id="tick">
+    <img src="delete.png" id ="delete" alt="delete" onclick="delete_it('task${no_of_tasks}','${dict.title}')">
+    </div>`
+    let colors=["Blue","Green","Red","Pink","Yellow","lightpink","black","grey","purple","cyan"]
+    let randomColor=colors[Math.floor(Math.random()*colors.length)];
+    checkbox=document.getElementById(`task${no_of_tasks}`)
+    checkbox.style.setProperty("--before-background-color",randomColor);
+    no_of_tasks+=1
+
+}
+
+async function refresh_tasks(){
+    const response = await fetch(`http://127.0.0.1:8000/my_tasks`);
+    const data = await response.json(); 
+    // console.log(data);
+    tasks=document.getElementById("tasks")
+    tasks.innerHTML=""
+    data.tasks.forEach(element => {
+        add_task_bh(element)
+    });
+
+
+}
+
+document.getElementById("refreshImg").addEventListener("click", function() {
+    this.classList.toggle("rotated"); 
+    refresh_tasks()
+});
+
